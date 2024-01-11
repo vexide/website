f7a9b1fe27bd330998c4277489f285c9bb22c26e
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-01-11 14:37:28 -0600

    setup integrations and tooling

---
+++ .prettierrc.mjs
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+export default {
+  useTabs: true,
+  tabWidth: 4,
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,23 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
+import prefetch from "@astrojs/prefetch";
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sitemap(), svelte(), prefetch()],
+  output: "hybrid",
+  vite: {
+    build: {
+      sourcemap: true
+    },
+    resolve: {
+      alias: {
+        "~": path.resolve("./src")
+      }
+    }
+  },
+  adapter: vercel()
+});
