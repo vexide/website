89f9b986f91de4f4472a131cb2ecb7ae04db3568
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-01-12 10:31:31 -0600

    fill in some placeholder links

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,10 @@
 export const CRATE_NAME = `pros`;
+
 export const GITHUB_ORG_URL = `https://github.com/pros-rs`;
 export const GITHUB_REPO_URL = `${GITHUB_ORG_URL}/pros-rs`;
+
+export const DISCORD_INVITE_CODE = `y9mcGuQRYz`;
+export const DISCORD_INVITE_URL = `https://discord.gg/y9mcGuQRYz`;
 
 export interface Example {
 	name: string;
