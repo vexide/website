f0da6f12bec24a3ac206f16eff0c7c933d50cdc2
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-01-11 20:06:09 -0600

    left side landing page

---
+++ src/components/Button.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+	export let variant: "default" | "accent-red" | "accent-yellow" = "default";
+	export let href = "";
+	export let disabled = false;
+
+	let className = "";
+	export { className as class };
+
+	export let element: HTMLElement | null = null;
+</script>
+
+<svelte:element
+	this={href && !disabled ? "a" : "button"}
+	bind:this={element}
+	role={href && !disabled ? "button" : undefined}
+	href={href && !disabled ? href : undefined}
+	class="button variant-{variant} {className}"
+	class:disabled
+	{...$$restProps}
+>
+	<slot />
+</svelte:element>
+
+<style>
+	.button {
+		user-select: none;
+		text-decoration: none;
+		display: inline-flex;
+		justify-content: center;
+		align-items: center;
+		box-sizing: border-box;
+		text-align: center;
+		font-family: inherit;
+		border-radius: 0;
+		font-family: var(--font-monospace);
+		font-size: 16px;
+		font-weight: 400;
+		padding-inline: 24px;
+		padding-block: 12px;
+		gap: 8px;
+		border: none;
+		/* text-transform: uppercase; */
+		transition: 0.15s ease;
+	}
+
+	.button.variant-default {
+		background-color: var(--background-secondary);
+		color: var(--foreground-primary);
+	}
+
+	.button.variant-accent-red {
+		background-color: var(--background-accent-red);
+		color: #fff;
+	}
+
+	.button.variant-accent-yellow {
+		background-color: var(--background-accent-yellow);
+		color: var(--background-primary);
+	}
+
+	.button.disabled {
+		cursor: not-allowed;
+		opacity: 0.6;
+	}
+</style>
