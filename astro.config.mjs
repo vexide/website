d7a5ad97eb52c19903066450be619a1394d2ed7b
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-01-11 19:45:44 -0600

    setup cf pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,25 @@
 import { defineConfig } from "astro/config";
-
 import path from "path";
-
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import prefetch from "@astrojs/prefetch";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://pros-rs.github.io",
-	base: "/website"
-	integrations: [sitemap(), svelte(), prefetch()],
-	vite: {
-		build: {
-			sourcemap: true,
-		},
-		resolve: {
-			alias: {
-				"~": path.resolve("./src"),
-			},
-		},
-	},
+  site: "https://pros-rs.pages.app/",
+  integrations: [sitemap(), svelte(), prefetch()],
+  output: "hybrid",
+  vite: {
+    build: {
+      sourcemap: true
+    },
+    resolve: {
+      alias: {
+        "~": path.resolve("./src")
+      }
+    }
+  },
+  adapter: cloudflare()
 });
