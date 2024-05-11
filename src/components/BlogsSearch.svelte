90987ea0e37b133c2828996545b8f740787a5c28
Gavin-Niederman <gavinniederman@gmail.com>
2024-05-11 10:44:19 -0700

    fix: make background extend to bottom of screen

--- src/components/BlogsSearch.svelte
+++ src/components/BlogsSearch.svelte
@@ -2,9 +2,10 @@
     import Button from "./Button.svelte";
     import Chip from "./Chip.svelte";
 
-    export let query = "";
     export let blogsFilter: boolean = true;
     export let releasesFilter: boolean = true;
+
+    let query: HTMLInputElement;
 
     interface Blog {
         title: string;
@@ -40,7 +41,7 @@
                     d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5A6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5S14 7.01 14 9.5S11.99 14 9.5 14"
                 /></svg
             >
-            <input bind:value={query} type="text" placeholder="Search" />
+            <input bind:this={query} type="text" placeholder="Search" />
         </div>
         <div class="chips">
             <Chip bind:activated={blogsFilter} text="Blogs" />
