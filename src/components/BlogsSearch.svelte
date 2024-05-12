eef23317f727dbd13f4437c0935baf705aecc378
Gavin-Niederman <gavinniederman@gmail.com>
2024-05-11 18:44:00 -0700

    feat: working blog search

--- src/components/BlogsSearch.svelte
+++ src/components/BlogsSearch.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
     import Button from "./Button.svelte";
     import Chip from "./Chip.svelte";
+    import fuzzysort from "fuzzysort";
 
     export let blogsFilter: boolean = true;
     export let releasesFilter: boolean = true;
 
-    let query: HTMLInputElement;
+    let query: string = "";
 
     interface Blog {
         title: string;
@@ -29,6 +30,13 @@
                 (blog.tag == "blog" && blogsFilter) ||
                 (blog.tag == "release" && releasesFilter)
         );
+        if (query != "") {
+            const results = fuzzysort.go(query, filteredBlogs, {
+                keys: ["title", "description", "author"],
+            });
+            console.log(results);
+            filteredBlogs = results.toSorted((a, b) => a.score - b.score).map((result) => result.obj);
+        }
     }
 </script>
 
@@ -41,7 +49,7 @@
                     d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5A6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5S14 7.01 14 9.5S11.99 14 9.5 14"
                 /></svg
             >
-            <input bind:this={query} type="text" placeholder="Search" />
+            <input bind:value={query} type="text" placeholder="Search" />
         </div>
         <div class="chips">
             <Chip bind:activated={blogsFilter} text="Blogs" />
