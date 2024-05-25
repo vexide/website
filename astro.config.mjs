5fd4bfafdc2261900b71324cd357a583f3327bba
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-05-25 09:56:54 -0500

    update page styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-import path from "path";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 
@@ -10,15 +9,5 @@ export default defineConfig({
 	site: "https://pros-rs.pages.dev/",
 	integrations: [sitemap(), svelte()],
 	output: "hybrid",
-	vite: {
-		build: {
-			sourcemap: true,
-		},
-		resolve: {
-			alias: {
-				"~": path.resolve("./src"),
-			},
-		},
-	},
 	adapter: cloudflare(),
 });
