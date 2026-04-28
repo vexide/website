674d44e1246f69139fa020075ee06ec18619ddb5
Lewis McClelland <lewis@lewismcclelland.me>
2026-04-27 20:20:38 -0400

    Use HTTP 301 for redirects
    This fixes `irm | iex` on Windows which doesn't know how to handle any http status codes created more recently than a decade ago.

--- src/pages/install.sh.ts
+++ src/pages/install.sh.ts
@@ -3,6 +3,6 @@ import type { APIRoute } from "astro";
 export const GET: APIRoute = ({ redirect }) => {
     return redirect(
         "https://github.com/vexide/cargo-v5/releases/latest/download/cargo-v5-installer.sh",
-        308,
+        301,
     );
 };
